17ef3d5e06c8ae1b59823d5914e2239a39f65f72
Klaudia Bras <bras.klaudia@gmail.com>
2023-10-13 23:56:08 +0200

    function welcome and onChangeBackground Click added

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,16 @@
-console.log("Cześć wszystkim! ")
+function welcome() {
+    console.log("Cześć wszystkim! ")
+}
+
+let onChangeBackgroundClick = () => {
+        body.classList.toggle("body__whiteBackground");
+        nextColorName.innerText = body.classList.contains("body__whiteBackground") ? "szare" : "białe";
+    };
+
+    welcome();
 
 let changeColorButton = document.querySelector(".section__button")
 let body = document.querySelector(".body")
 let nextColorName = document.querySelector(".section__button--change")
 
-changeColorButton.addEventListener("click", () => {
-    body.classList.toggle("body__whiteBackground");
-    nextColorName.innerText = body.classList.contains("body__whiteBackground") ? "szare" : "białe";
-});
+changeColorButton.addEventListener("click", onChangeBackgroundClick);
